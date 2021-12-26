bb4b1838089607b573f601d84536208f047d9265
yunseong9999 <dbsdltjd3701@naver.com>
2021-12-26 17:08:29 +0900

    feat : useEffectExample

--- frontend-study/src/UseEffectExample/UseEffectExample.js
+++ frontend-study/src/UseEffectExample/UseEffectExample.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
 
 function UseEffectExample() {
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
-    console.log("컴포넌트가 화면에 나타남");
+    console.log("컴포넌트가 화면에 나타남 ");
     return () => {
-      console.log("컴포넌트가 화면에서 사라짐");
+      console.log("컴포넌트가 화면에서 사라짐. 현재의 카운트 값:");
     };
-  }, []);
+  }, [count]);
+  console.log("rendering. 현재 카운트 값 : ");
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h1>Counter Example</h1>
+      {count}
+      <button onClick={increment}>Click</button>
+    </div>
+  );
 }
 
 export default UseEffectExample;
