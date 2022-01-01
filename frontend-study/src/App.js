00a3602e048207b1f84541b636a987d48ac67c43
yunseong9999 <dbsdltjd3701@naver.com>
2022-01-01 20:55:00 +0900

    feat : new study content

--- frontend-study/src/App.js
+++ frontend-study/src/App.js
@@ -1,43 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import InputName from "./InputExample/InputName";
-import RefExample from "./UseRefExample/RefExample";
-import StudentList from "./MappingExample/StudentList";
-import UseEffectExample from "./UseEffectExample/UseEffectExample";
-import CreateStudent from "./TreatArrayExample/CreateStudent";
+import ShowCustomerList from "./ProjectDirectory/ShowCustomerList";
 
 function App() {
-  const students = [
+  const customers = [
     {
+      id: 1,
       name: "이윤성",
-      id: 182761,
-      department: "소프트웨어공학",
-      email: "182761@jnu.ac.kr",
+      phoneNumber: "010-1111-1111",
     },
     {
-      name: "임xx",
-      id: 182848,
-      department: "전자공학",
-      email: "182848@jnu.ac.kr",
+      id: 2,
+      name: "김윤성",
+      phoneNumber: "010-2222-2222",
     },
     {
-      name: "김xx",
-      id: 181131,
-      department: "소프트웨어공학",
-      email: "181131@jnu.ac.kr",
+      id: 3,
+      name: "박윤성",
+      phoneNumber: "010-3333-3333",
     },
   ];
+  const [customerList, setCustomerList] = useState(customers);
+  
 
-  const onRemove = (id) => {
-    setStudents(students.filter((student) => student.id !== id));
-  };
   return (
     <div className="App">
-      <InputName />
-      <UseEffectExample />
-      <StudentList />
-      <button onClick={onRemove}>Remove</button>
+      <ShowCustomerList customerList={customerList} />
     </div>
   );
 }
---
+++ frontend-study/src/ProjectDirectory/InputCustomer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InputCustomer({ customerList }) {
+  return (
+    <div>
+      <input name="name" />
+    </div>
+  );
+}
+
+export default InputCustomer;
